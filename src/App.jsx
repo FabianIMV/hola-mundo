ea1d817754ae0ffd42d3267bc787599a05378871
FabianIMV <fabianimv@users.noreply.github.com>
2026-05-04 16:33:42 -0400

    Initial commit: Hola Mundo React app

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh', fontFamily: 'sans-serif' }}>
+      <h1>Hola Mundo 👋</h1>
+    </div>
+  )
+}
+
+export default App
